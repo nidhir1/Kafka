393282f70b45f7fbe1219265dbdbca90f2f52f70
nidhi r <nidhi.doit@gmail.com>
2025-04-16 20:05:20 -0400

    Update kafka_consumer.py

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,1 +1,14 @@
+from kafka import KafkaConsumer
+
+
+consumer = KafkaConsumer(
+    'my-topic', #topic name should be as in producer
+    bootstrap_servers='localhost:9092',
+    auto_offset_reset='earliest',
+    group_id='my-group'
+)
+
+for message in consumer:
+    data = message.value.decode('utf-8')
+    print(f"Received: {data}")
 
