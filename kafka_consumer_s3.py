5f885f907cd5c77ff9709ee7b220eddf2378ff41
nidhi r <nidhi.doit@gmail.com>
2025-04-16 20:02:25 -0400

    Update kafka_consumer_s3.py

--- kafka_consumer_s3.py
+++ kafka_consumer_s3.py
@@ -1,1 +1,17 @@
+from kafka import KafkaConsumer
+from kafka_write_to_s3 import write_to_s3
 
+consumer = KafkaConsumer(
+    'my-topic', #topic name should be as in producer
+    bootstrap_servers='localhost:9092',
+    auto_offset_reset='earliest',
+    group_id='my-group'
+)
+
+for message in consumer:
+    data = message.value.decode('utf-8')
+    print(f"Received: {data}")
+
+    # Write to  S3
+
+    write_to_s3(data)
